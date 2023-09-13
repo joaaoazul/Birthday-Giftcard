d42d36928530c0e76865fa48ce0d7ded4fa17bdd
joaao <joao.azul.11@outlook.com>
2023-09-13 13:25:46 +0100

    engine

---
+++ script.js
@@ -0,0 +1,6 @@
+function opacityZero (){
+    document.getElementById('btn').style.opacity = '0';
+    document.getElementById('masterContainer2').style.opacity = '1';
+    document.getElementById('masterContainer').style.opacity = '0';
+    
+}
